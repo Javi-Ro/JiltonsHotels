87ff5e631389e63f07cd4c0c2a8ab20a96053f4f
Madanusculus <madaniilicitano@gmail.com>
2021-04-15 18:44:49 +0200

    Discount and Package added

---
+++ Library/ENDiscount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class ENDiscount
+    {
+        private string _code;
+        public string code
+        {
+            get { return _code; }
+            set { _code = value; }
+        }
+
+        private int _percentage;
+        public int percentage
+        {
+            get { return _percentage; }
+            private set { _percentage = value; }
+        }
+
+
+        public ENDiscount(int code, int percentage)
+        {
+
+        }
+
+        public bool createDiscount()
+        {
+
+        }
+
+        public bool deleteDiscount()
+        {
+
+        }
+
+        public bool updatePercentage(int percentage)
+        {
+
+        }
+
+        const public ENBooking[] getBookings()
+        {
+
+        }
+
+
+    }
+}
