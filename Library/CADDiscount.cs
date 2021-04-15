85710556b9f691b6926fb6f4d91ea38a6f289315
Madanusculus <madaniilicitano@gmail.com>
2021-04-15 19:55:27 +0200

    CADDIsc and Package added

---
+++ Library/CADDiscount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class CADDiscount
+    {
+        string constring;
+
+        public CADDiscount()
+        {
+
+        }
+
+        public bool createDiscount(ENDiscount discount)
+        {
+
+        }
+
+        public bool deleteDiscount(ENDiscount discount)
+        {
+
+        }
+
+        public bool updatePercentage(ENDiscount discount)
+        {
+
+        }
+
+        public ENBooking[] getBookings(ENDiscount discount)
+        {
+
+        }
+
+
+    }
+}
---
+++ Library/CADPackage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class CADPackage
+    {
+        string constring;
+
+        public CADPackage(int id, ENService services, string name, string desc, int price)
+        {
+
+        }
+
+        public bool createPackage(ENPackage pack)
+        {
+
+        }
+
+        public bool deletePackage(ENPackage pack)
+        {
+
+        }
+
+        public bool searchPackage(ENPackage pack)
+        {
+
+        }
+
+        //SERVICES IN THE PACKAGE
+        public ENService[] getServices()
+        {
+
+        }
+
+        public bool addService(ENPackage pack, ENService serv)
+        {
+
+        }
+
+        public bool deleteService(ENPackage pack, ENService serv)
+        {
+
+        }
+
+        public bool includes(ENPackage pack, ENService serv)
+        {
+
+        }
+
+
+    }
+}
