62e3f37e2efedfc2682f244f7cd1f56f3fc88e42
Javier <jrj10@gcloud.ua.es>
2021-04-15 10:09:34 +0200

    Booking and Room classes updated with headers.

--- Library/ENBooking.cs
+++ Library/ENBooking.cs
@@ -8,5 +8,103 @@ namespace Library
 {
     public class ENBooking
     {
+        // Attributes and properties
+        private int id;
+
+        private ENUser _user;
+        public ENUser user
+        {
+            get { return _user; }
+            private set { _user = value; }
+        }
+
+        private ENDate _date;
+        public ENDate date
+        {
+            get { return _date; }
+            private set { _date = value; }
+        }
+
+        private bool _breakfast;  // It can change, by the moment the attribute is reflected here
+        public bool breakfast
+        {
+            get { return _breakfast; }
+            private set { _breakfast = value; }
+        }
+
+        // Booking methods
+        public Booking(ENUser user, ENDate date, ENRoom rooms[], bool breakfast)
+        {
+
+        }
+
+        public bool makeBooking()
+        {
+
+        }
+
+        public bool deleteBooking()
+        {
+
+        }
+
+        public bool updateBreakfast(bool breakfast)
+        {
+
+        }
+
+        // Rooms methods
+        const public ENRoom[] getRooms()
+        {
+
+        }
+
+        public bool addRoom(ENRoom room)
+        {
+
+        }
+
+        public bool deleteRoom(ENRoom room)
+        {
+
+        }
+
+        public bool isRoom(ENRoom room)
+        {
+
+        }
+        
+        // Services methods
+        const public ENService[] getServices()
+        {
+
+        }
+
+        public bool addService(ENService service)
+        {
+
+        }
+
+        public bool cancelService(ENService service)
+        {
+
+        }
+
+        // Discounts
+        public bool applyDiscount(ENDiscount discount)
+        {
+
+        }
+
+        public bool quitDiscount(ENDiscount discount)
+        {
+
+        }
+
+        // Auxiliary methods
+        const public int calculatePrice()
+        {
+
+        }
     }
 }
--- Library/ENRoom.cs
+++ Library/ENRoom.cs
@@ -8,6 +8,57 @@ namespace Library
 {
     class ENRoom
     {
+        private int _id;
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private string _type;
+        public string type
+        {
+            get { return _type; }
+            private set { _type = value; }
+        }
+
+        private int _price;
+        public int price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public ENRoom(int price, int type)
+        {
+
+        }
+
+        public bool createRoom()
+        {
+
+        }
+
+        public bool deleteRoom()
+        {
+
+        }
+
+        public bool updateType(string type)
+        {
+
+        }
+
+        public bool updatePrice(int price)
+        {
+
+        }
+
+        const public ENBooking[] getBookings()
+        {
+
+        }
+
 
     }
 }
