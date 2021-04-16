3723efc90708e35cb11ee86c3e8a8d01a7d703fd
adrianberenguer <aba111@gcloud.ua.es>
2021-04-16 19:54:55 +0200

    Adding Service classes.

---
+++ Library/CADService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Library
+{
+    public class CADService
+    {
+        private string constring;
+
+        public CADService()
+        {
+
+        }
+
+        public bool createService(ENService en)
+        {
+ 
+        }
+
+        public bool deleteService(ENService en)
+        {
+
+        }
+
+        public bool updateService(ENService en)
+        {
+
+        }
+
+    }
+}
--- Library/ENService.cs
+++ Library/ENService.cs
@@ -1,8 +1,92 @@
 ﻿using System;
 
-public class ENService
+namespace Library
 {
-	public ENService()
+	public class ENService
 	{
+		private int id;
+		private int price;
+		private string description;
+
+		private int Id
+		{
+			get
+			{
+				return id;
+			}
+			private set
+			{
+				id = value;
+			}
+		}
+
+		public int Price
+		{
+			get
+			{
+				return price;
+			}
+			private set
+			{
+				price = value;
+			}
+		}
+
+		private string Descritpion
+		{
+			get
+			{
+				return description;
+			}
+			private set
+			{
+				description = value;
+			}
+		}
+
+		public ENService()
+		{
+
+		}
+
+		private bool createService()
+        {
+
+        }
+
+		private bool deleteService()
+        {
+
+        }
+
+		private bool updateService()
+        {
+
+        }
+
+		public class Excursion : ENService
+        {
+			private string name;
+			private int maxPeople;
+
+        }
+
+		public class Gym : ENService
+        {
+
+        }
+
+		public class Spa : ENService
+		{
+
+		}
+		
+		public class Kingergarten : ENService
+        {
+
+        }
+
 	}
 }
+
+
