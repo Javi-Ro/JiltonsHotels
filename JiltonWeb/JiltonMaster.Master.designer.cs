b519c014787cfd5014207554b5e03225a3bfb016
ilyanSD <60897197+ilyanSD@users.noreply.github.com>
2021-04-28 19:29:45 +0200

    X7219873N Changes on transitions

--- JiltonWeb/JiltonMaster.Master.designer.cs
+++ JiltonWeb/JiltonMaster.Master.designer.cs
@@ -24,6 +24,15 @@ namespace JiltonWeb
         protected global::System.Web.UI.WebControls.ContentPlaceHolder TitlePage;
 
         /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
         /// ScriptManager1 control.
         /// </summary>
         /// <remarks>
