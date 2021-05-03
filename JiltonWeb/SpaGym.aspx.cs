2f11ee2e7cb487ad02ff372d915aab52da9a5126
adrianberenguer <aba111@gcloud.ua.es>
2021-05-03 19:45:50 +0200

    Final changes

--- JiltonWeb/SpaGym.aspx.cs
+++ JiltonWeb/SpaGym.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace JiltonWeb
 {
-    public partial class SpaGym : System.Web.UI.Page
+    public partial class SpaGym1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
