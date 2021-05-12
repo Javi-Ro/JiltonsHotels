e5657f7a39b2c04ae107e16a7b1da7dd361b6441
rmrl3 <rmrl3@alu.ua.es>
2021-05-12 10:34:05 +0200

    50383873G added buttons for admins in Restaurant

--- JiltonWeb/Restaurant.aspx.designer.cs
+++ JiltonWeb/Restaurant.aspx.designer.cs
@@ -49,5 +49,14 @@ namespace JiltonWeb
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label DESSERTS;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
     }
 }
