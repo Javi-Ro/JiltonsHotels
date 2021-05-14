2ec42438fc7abfca9252f7ae2adf916dd0e256a0
rmrl3 <rmrl3@alu.ua.es>
2021-05-14 23:47:36 +0200

    50383873G restaurant selects and create working

--- JiltonWeb/Restaurant.aspx.cs
+++ JiltonWeb/Restaurant.aspx.cs
@@ -12,101 +12,256 @@ namespace JiltonWeb
     {
         DateTime hoy = DateTime.Today.Date;
         DateTime copia = DateTime.Today.Date;
+        int i;
         protected void Page_Load(object sender, EventArgs e)
         {
             CultureInfo c = new CultureInfo("en-EN");
-            titulo.Text = DateTime.Today.ToString("dddd", c).ToUpper() + "'S MENU";
+            titulo.Text = DateTime.Today.ToString("dddd",c).ToUpper() + "'S MENU";
+            OnToday(sender, e);
         }
         protected void OnPrevious(object sender, EventArgs e)
         {
-            copia.AddDays(-1);
-
-            titulo.Text = "MENU OF " + copia.ToString();
+            i--;
+            copia = copia.AddDays(i);
+            titulo.Text = "MENU OF " + copia.ToString("dd'/'MM'/'yyyy");
             ENMenu menu = new ENMenu();
-            menu.fecha = copia.ToString();
+            menu.fecha = copia.ToString("yyyy'/'MM'/'dd");
 
             if (menu.showMenu())
             {
-                APPETIZERS.Text = menu.appetizers;
-                MAINS.Text = menu.main;
+                APPETIZERS.Text = i.ToString();
+                MAINS.Text = copia.ToString("dd'/'MM'/'yyyy");
                 DESSERTS.Text = menu.dessert;
-                PRICE.Text = menu.price.ToString();
+                PRICE.Text = menu.price.ToString() + "€";
             }
             else
             {
                 titulo.Text = "Menu not available";
                 meal1.Visible = false;
                 meal2.Visible = false;
-                meal2.Visible = false;
+                meal3.Visible = false;
             }
 
         }
 
         protected void OnNext(object sender, EventArgs e)
         {
-            copia.AddDays(1);        //se resetea
-
-            titulo.Text = "MENU OF " + copia.ToString();
+            i++;
+            copia = copia.AddDays(i);        
+            titulo.Text = "MENU OF " + copia.ToString("dd'/'MM'/'yyyy");
             ENMenu menu = new ENMenu();
-            menu.fecha = copia.ToString();
+            menu.fecha = copia.ToString("yyyy'/'MM'/'dd");
 
             if (menu.showMenu())
             {
                 APPETIZERS.Text = menu.appetizers;
                 MAINS.Text = menu.main;
                 DESSERTS.Text = menu.dessert;
-                PRICE.Text = menu.price.ToString();
+                PRICE.Text = menu.price.ToString() + "€";
             }
             else
             {
                 titulo.Text = "Menu not available";
                 meal1.Visible = false;
                 meal2.Visible = false;
-                meal2.Visible = false;
+                meal3.Visible = false;
             }
         }
 
         protected void OnToday(object sender, EventArgs e)
         {
-            copia = hoy;        //se resetea
-
+            i = 0;
+            copia = hoy;
             ENMenu menu = new ENMenu();
 
-            menu.fecha = DateTime.Today.Date.ToString();
+            menu.fecha = DateTime.Today.Date.ToString("yyyy'/'MM'/'dd");
 
             if (menu.showMenu())
             {
                 APPETIZERS.Text = menu.appetizers;
                 MAINS.Text = menu.main;
                 DESSERTS.Text = menu.dessert;
-                PRICE.Text = menu.price.ToString();
+                PRICE.Text = menu.price.ToString() + "€";
             }
             else
             {
                 titulo.Text = "Menu not available";
                 meal1.Visible = false;
                 meal2.Visible = false;
-                meal2.Visible = false;
+                meal3.Visible = false;
+            }
+
+        }
+
+        protected void OnCreate(object sender, EventArgs e)
+        {
+            success.Visible = false;
+            Error.Visible = false;
+            if(String.IsNullOrEmpty(DateTB.Text))
+            {
+                ErrorDate.Visible = true;
+            }
+            if (String.IsNullOrEmpty(appetizersTB.Text))
+            {
+                ErrorApp.Visible = true;
+            }
+            if (String.IsNullOrEmpty(mainTB.Text))
+            {
+                ErrorMain.Visible = true;
+            }
+            if (String.IsNullOrEmpty(dessertTB.Text))
+            {
+                ErrorDessert.Visible = true;
+            }
+            if (String.IsNullOrEmpty(priceTB.Text))
+            {
+                ErrorPrice.Visible = true;
+            }
+            
+            if(!String.IsNullOrEmpty(DateTB.Text) && !String.IsNullOrEmpty(appetizersTB.Text) && !String.IsNullOrEmpty(mainTB.Text) && !String.IsNullOrEmpty(dessertTB.Text) && !String.IsNullOrEmpty(priceTB.Text)){
+                string[] aux = new string[3];
+                aux = DateTB.Text.Split('/');
+                string fechaFormateada = aux[2] + "/" + aux[1] + "/" + aux[0];
+                ENMenu menu = new ENMenu(mainTB.Text, dessertTB.Text, appetizersTB.Text, float.Parse(priceTB.Text), fechaFormateada);
+                
+                if (menu.create())
+                {
+                    success.Text = "Menu of day " + DateTB.Text + " inserted succesfully ";
+                    success.Visible = true;
+                }
+
+                else
+                {
+                    Error.Visible = true;
+                    Error.Text = "Could not insert menu ";
+                }
             }
 
         }
 
         protected void OnUpdate(object sender, EventArgs e)
         {
-            ENMenu menu = new ENMenu();
-            menu.fecha = DateTB.ToString();     //espero que funcione
+            success.Visible = false;
+            Error.Visible = false;
+            if (String.IsNullOrEmpty(DateTB.Text))
+            {
+                ErrorDate.Visible = true;
+            }
 
+            if (!String.IsNullOrEmpty(DateTB.Text))
+            {
+                string[] aux = new string[3];
+                aux = DateTB.Text.Split('/');
+                string fechaFormateada = aux[2] + "/" + aux[1] + "/" + aux[0];
+                ENMenu menu = new ENMenu(mainTB.Text, dessertTB.Text, appetizersTB.Text, float.Parse(priceTB.Text), fechaFormateada);
 
-        }
+                if (menu.update() && (!String.IsNullOrEmpty(appetizersTB.Text) || !String.IsNullOrEmpty(mainTB.Text) && !String.IsNullOrEmpty(dessertTB.Text) || !String.IsNullOrEmpty(priceTB.Text)))      //at least one field must be known
+                {
+                    success.Text = "Menu of day " + DateTB.Text + " updated succesfully ";
+                    success.Visible = true;
+                }
 
-        protected void OnInsert(object sender, EventArgs e)
-        {
-
+                else
+                {
+                    Error.Visible = true;
+                    Error.Text = "Could not update menu ";
+                }
+            }
         }
 
         protected void OnDelete(object sender, EventArgs e)
-        { 
-        
+        {
+            success.Visible = false;
+            Error.Visible = false;
+            ENMenu menu = new ENMenu();
+            if (String.IsNullOrEmpty(DateTB.Text))
+            {
+                ErrorDate.Visible = true;
+            }
+            else
+            {
+                if (menu.delete())
+                {
+                    success.Visible = true;
+                    success.Text = menu.dessert;
+                    success.Text = "Menu of day " + DateTB.Text + " deleted succesfully ";
+                    
+                }
+
+                else
+                {
+                    Error.Visible = true;
+                    Error.Text = menu.dessert; /*"Could not delete menu ";*/
+                }
+            }
+
+
+        }
+
+        protected void DeletePage(object sender, EventArgs e)
+        {
+            ErrorDate.Visible = false;
+            ErrorDessert.Visible = false;
+            ErrorApp.Visible = false;
+            ErrorPrice.Visible = false;
+            ErrorMain.Visible = false;
+            mainLabel.Visible = false;
+            mainTB.Visible = false;
+            appetizersTB.Visible = false;
+            appetizersLabel.Visible = false;
+            dessertTB.Visible = false;
+            dessertLabel.Visible = false;
+            priceLabel.Visible = false;
+            priceTB.Visible = false;
+            Create.Visible = false;
+            Delete.Visible = true;
+            Update.Visible = false;
+            success.Visible = false;
+            Error.Visible = false;
+        }
+
+        protected void UpdatePage(object sender, EventArgs e)
+        {
+            ErrorDate.Visible = false;
+            ErrorDessert.Visible = false;
+            ErrorApp.Visible = false;
+            ErrorPrice.Visible = false;
+            ErrorMain.Visible = false;
+            mainLabel.Visible = true;
+            mainTB.Visible = true;
+            appetizersTB.Visible = true;
+            appetizersLabel.Visible = true;
+            dessertTB.Visible = true;
+            dessertLabel.Visible = true;
+            priceLabel.Visible = true;
+            priceTB.Visible = true;
+            Create.Visible = false;
+            Update.Visible = true;
+            Delete.Visible = false;
+            success.Visible = false;
+            Error.Visible = false;
+        }
+
+        protected void InsertPage(object sender, EventArgs e)
+        {
+            ErrorDate.Visible = false;
+            ErrorDessert.Visible = false;
+            ErrorApp.Visible = false;
+            ErrorPrice.Visible = false;
+            ErrorMain.Visible = false;
+            Create.Visible = true;
+            Update.Visible = false;
+            Delete.Visible = false;
+            mainLabel.Visible = true;
+            mainTB.Visible = true;
+            appetizersTB.Visible = true;
+            appetizersLabel.Visible = true;
+            dessertTB.Visible = true;
+            dessertLabel.Visible = true;
+            priceLabel.Visible = true;
+            priceTB.Visible = true;
+            success.Visible = false;
+            Error.Visible = false;
         }
     }
 }
---
+++ JiltonWeb/css/About.aspx
@@ -0,0 +1,78 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/JiltonMaster.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="JiltonWeb.About" %>
+<asp:Content ID="cssLink" ContentPlaceHolderID="cssLink" runat="server">
+    <link rel="stylesheet" href="../css/about.css?ver=<?php echo rand(156,999)?>" />
+    <link href="https://fonts.googleapis.com/css2?family=IM+Fell+Double+Pica:ital@1&display=swap" rel="stylesheet">
+    <style type="text/css">
+      
+    </style>
+
+     <meta http-equiv="Expires" content="0">
+    <meta http-equiv="Last-Modified" content="0">
+    <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="History" >
+        <br />
+        <h1 >Our History</h1>
+        <br />
+        <div class="text">
+            
+                <p>As with any good story, the tale of the Jilton Hotel
+                    starts long before raising its walls; it begins in January 1971,
+                    when Dr. Javier Rodriguez moved to Rarotonga, Cook Islands and assumed responsibility 
+                    for the Rarotonga Hospital.</p>
+            
+        </div>
+        <div class="text">
+            <p>But Rarotonga was a thriving community long before his arrival, and, 
+                of course, also home of the Hon-lu-lu First Nations long, 
+                long before that.</p>
+            <br />
+            <h2>A rich history</h2>
+            <br />
+            <p>For several thousand years, the Tla-na-napi-tho Nation (anglicized to Tokerau)
+                have lived and settled in the region, including the village of Opitsaht, 
+                across the water from Rarotonga. A nation of skilled fisherman 
+                and master carvers, the Tla-na-napi-tho were once led by Chief 
+                Wickaninnish—from whom the Inn gets its name (meaning “No one goes before 
+                him” or “He-who-no-one-sits-in-front-of-in-the-canoe,” depending). 
+                Both feared and revered as a courageous leader, Wickaninnish led his 
+                warriors to several victories against neighbouring nations, gaining command 
+                of a wide swath of territory by the end of the 18th century encompassing an 
+                estimated 13,000 First Nations people.</p>
+            <br /><br />
+            <div class="foto">
+            <img src="assets/Rarotonga.jpg"  />
+            </div>
+            <br /><br />
+
+        </div>
+        <div class="text-editor">
+            <h2>Opening doors</h2>
+            <p>Back when the resort was created, an old Inn at the south end of Tikioki 
+                Beach had been closed and turned into an interpretive centre 
+                 Dr. Javirru had often thought about recreating it anew. 
+                In the ‘90s, inspired by their parents' foresight, Mario and Peque 
+                Javi joined with a group of long-time Cook residents to make 
+                the vision a reality.</p>
+            <p>On the family property, in the area where, as kids, they’d run to watch 
+                the storms come in, Mario and Peque cleared the footprint for the 
+                building. They worked by hand to ensure no trees were taken down 
+                unnecessarily, and then started construction. In 1996 the Tikioki 
+                Inn and the Mori Restaurant opened at the north end of Aroa Beach, 
+                and two years later, in 1998, they opened the Jilton Spa.</p>
+            <p>It was a welcome addition for visitors coming to Rorotonga. 
+                With the popularity of wildlife sightings, surfing and storm watching, 
+                the area had become a destination and the quality experience provided by 
+                the Inn brought a steady growth in international visitors.</p>
+            <br /><br />
+            <a href="https://www.google.com/maps/place/Rarotonga,+Islas+Cook/data=!4m2!3m1!1s0x7157d168aca32ae9:0x51a001c47eff9e5d?sa=X&ved=2ahUKEwjnuOH5oK7wAhW2CGMBHXj2DjoQ8gEwKHoECFQQAQ" style="color: black;">
+                <h1>We are here</h1>
+                </a>
+            <br />
+            
+        </div>
+        </div>
+        
+</asp:Content>
